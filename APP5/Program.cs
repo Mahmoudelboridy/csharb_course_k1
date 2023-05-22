576dafc17c4bfb9f2714bdc0dd3118c123ed98d2
Mahmoudelboridy <111857983+Mahmoudelboridy@users.noreply.github.com>
2023-05-23 01:25:15 +0300

    Add files via upload

---
+++ APP5/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+namespace app1
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("enter first rakm");
+
+            int n1 = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("enter tany rakm");
+
+            double n2 = Convert.ToDouble(Console.ReadLine());
+
+            Console.WriteLine($"magmo3 is {n1+n2}");
+            Console.WriteLine($"tar7 is {n1 - n2}");
+            Console.WriteLine($"darb is {n1 * n2}");
+            Console.WriteLine($"magmo3 is {n1 / n2}");
+
+
+        }
+    }
+
+
+}
