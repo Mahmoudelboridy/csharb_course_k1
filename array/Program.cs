b823b911def8bac7b4dc7bb02abf16077aa4a4a8
Mahmoudelboridy <111857983+Mahmoudelboridy@users.noreply.github.com>
2023-05-23 01:26:43 +0300

    Add files via upload

---
+++ array/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+namespace app1
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] an = { 1, 7, 3, 4 };
+            Console.WriteLine(an[1]);
+            double[] ann = { 1.3,1.2,1.4,1.7};
+            Console.WriteLine(ann[3]);
+            string[] annn = { "ahmed", "hassen","ali" };
+            Console.WriteLine(annn[1]);
+            annn[1] = "van";
+            Console.WriteLine(annn[1]);
+
+
+
+        }
+    }
+}
+
