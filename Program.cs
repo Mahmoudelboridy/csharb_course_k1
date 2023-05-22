fc20e6c6decabca3c37e6819c1c25f35a9025411
Mahmoudelboridy <111857983+Mahmoudelboridy@users.noreply.github.com>
2023-05-23 01:18:30 +0300

    Add files via upload

--- Program.cs
+++ Program.cs
@@ -1,17 +1,26 @@
 ﻿using System;
 namespace app1
-    {
+{
     public class Program
     {
         static void Main(string[] args)
         {
-            Console.ForegroundColor = ConsoleColor.Black;
-            Console.BackgroundColor = ConsoleColor.Yellow;
-            Console.Clear();
-            Console.WriteLine("man city will win isa");
+        
+
+            for(int i=0; i<=7;i++)
+            {
+                Console.WriteLine(i);
+            }
+            Console.WriteLine("fasla");
+
+            int[] am = { 1, 2, 5, 77, 9, 61, 16, 44, 42, 47 };
+            for (int f = 0; f< am.Length; f++)
+            {
+                Console.WriteLine(am[f]);
+            }
 
         }
     }
 
 
-    }
+}
