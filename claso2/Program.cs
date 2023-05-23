7f3ce6cd59cf0244f87e78d013e0dd28ec202337
Mahmoudelboridy <111857983+Mahmoudelboridy@users.noreply.github.com>
2023-05-24 01:25:04 +0300

    Add files via upload

---
+++ claso2/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.ComponentModel.Design;
+using System.Runtime.InteropServices;
+
+namespace build_method
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            animal dog = new animal("black","jony",.5);
+            dog.print();
+        }
+      class animal
+        {
+            string _color;
+            string _name;
+            double _tall;
+            public animal(string color, string name,double tall)
+            {
+                this._color = color;
+                this._tall = tall;
+                this._name = name;
+
+            }
+            public void print()
+            {
+                Console.WriteLine($"its name is {_name} and its color is {_color} and its tall is {_tall}");
+
+            }
+    }
+        
+
+        }
+    }
+
+
+
