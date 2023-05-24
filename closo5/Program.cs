7118d7ee8949ecf3f92866efa0cb2d412123270f
Mahmoudelboridy <111857983+Mahmoudelboridy@users.noreply.github.com>
2023-05-24 05:14:33 +0300

    Add files via upload

---
+++ closo5/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+namespace build_method
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            Vehicle toyta = new Vehicle();
+            toyta.honk();
+
+            Car myCar = new Car();
+
+            myCar.honk();
+
+            Console.WriteLine(myCar.brand + " " + myCar.modelName);
+
+        }
+        class Vehicle   
+        {
+            public string brand = "Ford"; 
+            public void honk()             
+            {
+                Console.WriteLine("Tuut, tuut! my vihicle is "+brand);
+            }
+        }
+        class Car : Vehicle 
+        {
+            public string modelName = "corla";
+        }
+
+
+
+
+
+    }
+}
+
