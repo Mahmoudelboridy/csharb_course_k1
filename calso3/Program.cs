337edfbe9fe72b084b6c48bf342cbd822cff155b
Mahmoudelboridy <111857983+Mahmoudelboridy@users.noreply.github.com>
2023-05-24 03:59:01 +0300

    Add files via upload

---
+++ calso3/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.ComponentModel.Design;
+using System.Runtime.InteropServices;
+
+namespace build_method
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            circle done = new circle(5);
+            float area = done.Printf();
+            Console.WriteLine(area);
+        }
+        class circle
+        {
+            float  _pi=3.14f;
+            int _raduis;
+
+           
+            public circle(int raduis)
+            {
+                _raduis = raduis;
+            }
+            public float Printf()
+            {
+                return this._raduis * this._pi;
+            }
+        }
+
+
+    }
+}
+
+
