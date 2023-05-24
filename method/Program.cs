797c5594478286dade3ac0c79f3007b575eee5ab
Mahmoudelboridy <111857983+Mahmoudelboridy@users.noreply.github.com>
2023-05-24 22:59:09 +0300

    Add files via upload

---
+++ method/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.ComponentModel.Design;
+using System.Runtime.InteropServices;
+
+class ExtensionMethods2
+{
+
+    static void Main()
+    {
+        int[] ints = { 10, 45, 15, 39, 21, 26 };
+        var result = ints.OrderBy(g => g);
+        foreach (var i in result)
+        {
+            System.Console.Write(i + " ");
+        }
+    }
+}
